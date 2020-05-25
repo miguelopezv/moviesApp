0998c5a0b7585e0a62ae67ef1a55c8a55f07541c
miguelopezv <miguelopezv@gmail.com>
2020-05-25 18:35:33 -0500

    swiper and footer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:movies/providers/movies_providers.dart';
 import 'package:movies/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
+  final _moviesProvider = new MoviesProvider();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,15 +21,51 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         child: Column(
-          children: <Widget>[_cardSwiper()],
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[_cardSwiper(), _footer(context)],
         ),
       ),
     );
   }
 
   Widget _cardSwiper() {
-    return CardSwiperWidget(
-      list: [1, 2, 3, 4, 5],
+    return FutureBuilder(
+      future: _moviesProvider.getNowPlaying(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          return CardSwiperWidget(
+            list: snapshot.data,
+          );
+        } else {
+          return Container(
+              height: 400,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ));
+        }
+      },
+    );
+  }
+
+  Widget _footer(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Most popular',
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+          FutureBuilder(
+            future: _moviesProvider.getPopular(),
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+              snapshot.data.forEach((element) {});
+
+              return Container();
+            },
+          ),
+        ],
+      ),
     );
   }
 }
