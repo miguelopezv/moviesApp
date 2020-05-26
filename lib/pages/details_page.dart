836d88e9f66040535d24c80c88108db490566011
miguelopezv <miguelopezv@gmail.com>
2020-05-26 17:07:39 -0500

    add title and description

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -8,7 +8,18 @@ class DetailsPage extends StatelessWidget {
 
     return Scaffold(
       body: CustomScrollView(
-        slivers: <Widget>[_appBar(movie)],
+        slivers: <Widget>[
+          _appBar(movie),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 10,
+              ),
+              _posterTitle(context, movie),
+              _movieDescription(movie)
+            ]),
+          )
+        ],
       ),
     );
   }
@@ -34,6 +45,53 @@ class DetailsPage extends StatelessWidget {
           fadeInDuration: Duration(milliseconds: 150),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+
+  Widget _posterTitle(BuildContext context, Movie movie) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image(
+              image: NetworkImage(movie.getPosterImg()),
+              height: 159,
+            ),
+          ),
+          SizedBox(width: 20),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  movie.title,
+                  style: Theme.of(context).textTheme.headline6,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star_border),
+                    Text(movie.voteAverage.toString(),
+                        style: Theme.of(context).textTheme.subtitle1)
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _movieDescription(Movie movie) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+      child: Text(
+        movie.overview,
+        textAlign: TextAlign.justify,
       ),
     );
   }
